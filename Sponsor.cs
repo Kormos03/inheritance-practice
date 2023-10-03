ac2a8bcb72bd0683d28f1d8ce2a995cb07fdaeb7
Kormos Áron <104300544+Kormos03@users.noreply.github.com>
2023-10-03 19:51:25 +0200

    Sponsor,Mentor,Student,Person classes are done

--- Sponsor.cs
+++ Sponsor.cs
@@ -11,6 +11,15 @@ namespace inheritance_10_03
         private string company;
         private int hiredStudents;
 
+        public Sponsor(string name, int age, string gender, string company, int hiredStudents) : base(name,age,gender)
+        {
+            this.hiredStudents = 0;
+            this.Name = "Jane Doe";
+            this.Age = 30;
+            this.company = "Google";
+            this.Gender = "female";
+        }
+
         public override void introduce()
         {
             Console.WriteLine($"Hi, Im {Name}, a {Age} year old {Gender} who represents {company} and hied {hiredStudents} students so far.");
